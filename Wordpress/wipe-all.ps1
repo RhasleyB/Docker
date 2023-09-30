7f49a4263812cc96c452820932f4580d444e7124
rhasleyb <rhasley.biangulo@outlook.com>
2023-09-30 10:42:28 -0400

    Fix Errors

--- Wordpress/wipe-all.ps1
+++ Wordpress/wipe-all.ps1
@@ -1,9 +1,9 @@
 # Remove all files of "data"
 Remove-Item -Recurse -Force data\*
 
-# Remove all files of "www"
-Remove-Item -Recurse -Force www\*
+# Remove all files of "app"
+Remove-Item -Recurse -Force app\*
 
 # Pause and echo a message "Done", and wait for user to press a key
-Write-Host "Done! All files in data and www are removed."
+Write-Host "Done! All files in data and app are removed."
 Read-Host -Prompt "Press Enter to exit"
